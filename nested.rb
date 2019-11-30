07cbdcc2bf5550e7961d40939950801563bd8e45
Henry Phan <h.phan06@gmail.com>
2019-11-30 00:44:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash.first = {:yukihiro_matsumoto => {}}
+programmer_hash.push = {:yukihiro_matsumoto => {}}
     
 end
 
